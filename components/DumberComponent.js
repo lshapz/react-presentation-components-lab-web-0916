86346eb095cda772cdda3a295599c9801ba7d085
lshapz <laurashapiro01@gmail.com>
2016-11-19 16:04:42 -0500

    Done.

---
+++ components/DumberComponent.js
@@ -0,0 +1,17 @@
+const React = require('react');
+
+export default function 
+
+DumberComponent(props){
+  
+
+  return (
+    <div onClick={props.handleClick}>
+    I am just happy.
+    </div> 
+    )
+
+}
+
+module.exports = DumberComponent;
+
--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const ReactDOM = require('react-dom');
 const DumbComponent = require('./components/DumbComponent');
 const DumberComponent = require('./components/DumberComponent');
 
+function poop(){
+  console.log('dumbest')
+}
+
 ReactDOM.render(
   <div>
     <DumbComponent />
-    <DumberComponent handleClick={undefined} />
+    <DumberComponent handleClick={poop} />
   </div>,
   document.getElementById('main')
 );
